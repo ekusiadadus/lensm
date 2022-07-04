8650ae1711556e69d088e8c5be1014e99f4a72e2
Egon Elbre <egonelbre@gmail.com>
2022-07-04 15:21:07 +0300

    implement auto-refresh, filtering and selectbox

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 	"gioui.org/app"
 	"gioui.org/font/gofont"
+	"gioui.org/io/key"
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
@@ -20,6 +21,16 @@ import (
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
+
+	"loov.dev/lensm/internal/f32color"
+)
+
+var (
+	HeaderHeightRem  = unit.Sp(1.5)
+	HeaderBackground = f32color.NRGBAHex(0xAAAAAAFF)
+
+	SecondaryBackground = color.NRGBA{R: 0xF0, G: 0xF0, B: 0xF0, A: 0xFF}
+	SplitterBackground  = color.NRGBA{R: 0x80, G: 0x80, B: 0x80, A: 0xFF}
 )
 
 const N = 44
@@ -113,6 +124,9 @@ func main() {
 type UI struct {
 	Theme *material.Theme
 
+	AutoRefresh widget.Bool
+
+	Filter   widget.Editor
 	Output   *Output
 	Matches  widget.List
 	Selected *Match
@@ -123,6 +137,9 @@ func NewUI() *UI {
 	ui := &UI{}
 	ui.Theme = material.NewTheme(gofont.Collection())
 	ui.Matches.List.Axis = layout.Vertical
+
+	ui.Filter.SetText("gioui.org.*decode")
+	ui.Filter.SingleLine = true
 	return ui
 }
 
@@ -150,38 +167,95 @@ func (ui *UI) Layout(gtx layout.Context) {
 	}
 
 	layout.Flex{
-		Axis: layout.Horizontal,
+		Axis: layout.Vertical,
 	}.Layout(gtx,
 		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-			size := gtx.Constraints.Max
-			gtx.Constraints = layout.Exact(image.Point{
-				X: gtx.Metric.Sp(10 * 20),
-				Y: gtx.Constraints.Max.Y,
-			})
-			paint.FillShape(gtx.Ops, secondaryBackground, clip.Rect{Max: size}.Op())
-			return ui.layoutMatches(gtx)
-		}),
-		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-			size := image.Point{
-				X: gtx.Metric.Dp(1),
-				Y: gtx.Constraints.Max.Y,
-			}
-			paint.FillShape(gtx.Ops, splitterColor, clip.Rect{Max: size}.Op())
-			return layout.Dimensions{
-				Size: size,
-			}
+			return layout.Flex{
+				Axis:      layout.Horizontal,
+				Alignment: layout.Middle,
+			}.Layout(gtx,
+				layout.Rigid(layout.Spacer{Width: 8}.Layout),
+				layout.Flexed(1,
+					func(gtx layout.Context) layout.Dimensions {
+						return widget.Border{
+							Color: ui.Theme.Fg,
+							Width: 1,
+						}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+							return layout.UniformInset(2).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+								editor := material.Editor(ui.Theme, &ui.Filter, "Filter")
+								return editor.Layout(gtx)
+							})
+						})
+					}),
+				layout.Rigid(layout.Spacer{Width: 8}.Layout),
+				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+					_, err := regexp.Compile(ui.Filter.Text())
+					if err != nil {
+						body1 := material.Body1(ui.Theme, err.Error())
+						body1.Color = color.NRGBA{R: 0xC0, A: 0xFF}
+						body1.MaxLines = 1
+						return body1.Layout(gtx)
+					}
+					body1 := material.Body1(ui.Theme, "ok")
+					body1.MaxLines = 1
+					return body1.Layout(gtx)
+				}),
+				layout.Rigid(layout.Spacer{Width: 8}.Layout),
+				layout.Rigid(
+					material.CheckBox(ui.Theme, &ui.AutoRefresh, "Auto Refresh").Layout,
+				),
+				layout.Rigid(layout.Spacer{Width: 8}.Layout),
+			)
 		}),
 		layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-			gtx.Constraints = layout.Exact(gtx.Constraints.Max)
-			if ui.Selected == nil {
-				return material.H4(ui.Theme, "nothing selected").Layout(gtx)
-			}
-			return ui.layoutCode(gtx, ui.Selected)
+			return layout.Flex{
+				Axis: layout.Horizontal,
+			}.Layout(gtx,
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					size := gtx.Constraints.Max
+					gtx.Constraints = layout.Exact(image.Point{
+						X: gtx.Metric.Sp(10 * 20),
+						Y: gtx.Constraints.Max.Y,
+					})
+					paint.FillShape(gtx.Ops, SecondaryBackground, clip.Rect{Max: size}.Op())
+					return ui.layoutMatches(gtx)
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					size := image.Point{
+						X: gtx.Metric.Dp(1),
+						Y: gtx.Constraints.Max.Y,
+					}
+					paint.FillShape(gtx.Ops, SplitterBackground, clip.Rect{Max: size}.Op())
+					return layout.Dimensions{
+						Size: size,
+					}
+				}),
+				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+					gtx.Constraints = layout.Exact(gtx.Constraints.Max)
+					if ui.Selected == nil {
+						return material.H4(ui.Theme, "nothing selected").Layout(gtx)
+					}
+					return ui.layoutCode(gtx, ui.Selected)
+				}),
+			)
 		}),
 	)
 }
 
+type ListBox struct {
+	focused bool
+	active  int
+	scroll  widget.List
+}
+
 func (ui *UI) layoutMatches(gtx layout.Context) layout.Dimensions {
+	defer clip.Rect{Max: gtx.Constraints.Min}.Push(gtx.Ops).Pop()
+
+	key.InputOp{
+		Tag:  123,
+		Keys: key.NameUpArrow + "|" + key.NameDownArrow,
+	}.Add(gtx.Ops)
+
 	n := len(ui.Output.Matches)
 	if ui.Output.More {
 		n += 1
@@ -192,6 +266,25 @@ func (ui *UI) layoutMatches(gtx layout.Context) layout.Dimensions {
 		for match.Select.Clicked() {
 			ui.selectMatch(match)
 		}
+	}
+
+	var focusOffset int
+	for _, ev := range gtx.Events(123) {
+		fmt.Printf("%#v\n", ev)
+		if ev, ok := ev.(key.Event); ok {
+			if ev.State != key.Press {
+				continue
+			}
+			switch ev.Name {
+			case key.NameUpArrow:
+				focusOffset--
+			case key.NameDownArrow:
+				focusOffset++
+			}
+		}
+	}
+	if focusOffset != 0 {
+		fmt.Println("focus offset changed")
 	}
 
 	return material.List(ui.Theme, &ui.Matches).Layout(gtx, n,
@@ -241,8 +334,3 @@ func (ui *UI) layoutCode(gtx layout.Context, match *Match) layout.Dimensions {
 		LineHeight:   unit.Sp(14),
 	}.Layout(gtx)
 }
-
-var (
-	secondaryBackground = color.NRGBA{R: 0xF0, G: 0xF0, B: 0xF0, A: 0xFF}
-	splitterColor       = color.NRGBA{R: 0x80, G: 0x80, B: 0x80, A: 0xFF}
-)
